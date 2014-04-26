ead322cfdcad4d0bfe45766d37854edf98c853ba
robodylan <robodylan123@gmail.com>
2014-04-26 11:29:02 -0400

    We Have A Window
    Added window and window events. Also added drawing class.

--- LD-29/Game.cs
+++ LD-29/Game.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 //Import SFML
 using SFML.Audio;
 using SFML.Graphics;
@@ -12,14 +14,38 @@ namespace LD_29
 {
     class Game
     {
+        //Handle Window Close
+            public static void OnClose(object sender, EventArgs e){
+                ((RenderWindow)sender).Close();
+            }
+
         public static string Title = "One Who Shall Not Be Named Yet";
         public static void Start()
         {
+            //Find Screen boundries.
+            Rectangle r = Screen.PrimaryScreen.Bounds;
             //Create Window
-            RenderWindow window = new RenderWindow(new VideoMode(800,600),Title);
+            RenderWindow window = new RenderWindow(new VideoMode((uint)(r.Width * 0.9f), (uint)(r.Height * 0.9f)), Title);
+            window.Closed += new EventHandler(OnClose);
+            window.SetFramerateLimit(120);
+            //Load All Assets Here
 
+            //Begin Main Loop
+            while (window.IsOpen())
+            {
+                //Check For Window Events
+                window.DispatchEvents();
+                
+                //Draw On Window
+                Draw();
+                //Update window
+            }
 
-            
+        }
+
+        //Method for drawing all assets
+        public static void Draw()
+        {
 
         }
     }
